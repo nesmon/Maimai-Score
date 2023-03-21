525e96617ab1fae64057a37a8ab9f4aa6c8300a8
nenes <nesmon2108@protonmail.com>
2023-03-21 22:10:38 +0100

    Setup base route with base firebase call

---
+++ src/Route/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import './NotFound.css';
+
+function NotFound() {
+    return (
+        <div className="App">
+             <p>404</p>
+        </div>      
+    );
+}
+
+export default NotFound;
---
+++ src/config.example.js
@@ -0,0 +1,13 @@
+const config = {
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+    }
+}
+
+export default config
